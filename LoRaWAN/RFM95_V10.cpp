54cf916f57bab2354f0d7036c35a6813e7f162eb
Gerben <gerben@ideetron.nl>
2015-10-23 15:23:17 +0200

    Create RFM95_V10.cpp

---
+++ LoRaWAN/RFM95_V10.cpp
@@ -0,0 +1,183 @@
+/******************************************************************************************
+*
+* File:        RFM95_V10.cpp
+* Author:      Gerben den Hartog
+* Compagny:    Ideetron B.V.
+* Website:     http://www.ideetron.nl/LoRa
+* E-mail:      info@ideetron.nl
+******************************************************************************************/
+
+/****************************************************************************************
+*
+* Created on:         23-10-2015
+* Supported Hardware: ID150119-02 Nexus board with RFM95
+*
+* History:
+*
+* Firmware version: 1.0
+* First version for LoRaWAN
+****************************************************************************************/
+
+#include <SPI.h>
+#include "RFM95_V10.h"
+#include "LoRaWAN_V20.h"
+
+/*
+*****************************************************************************************
+* Description: Function used to initialize the RFM module on startup
+*****************************************************************************************
+*/
+
+void RFM_Init()
+{
+  //Switch RFM to sleep
+  RFM_Write(0x01,0x00);
+
+  //Set RFM in LoRa mode
+  RFM_Write(0x01,0x80);
+
+  //Set RFM in Standby mode wait on mode ready
+  RFM_Write(0x01,0x81);
+  while (digitalRead(DIO5) == LOW)
+  {
+  }
+
+  //Set carrair frequency
+  // 868.100 MHz / 61.035 Hz = 14222987 = 0xD9068B
+  RFM_Write(0x06,0xD9);
+  RFM_Write(0x07,0x06);
+  RFM_Write(0x08,0x8B);
+
+  //PA pin (maximal power)
+  RFM_Write(0x09,0xFF);
+
+  //BW = 125 kHz, Coding rate 4/5, Explicit header mode
+  RFM_Write(0x1D,0x72);
+
+  //Spreading factor 7, PayloadCRC On
+  RFM_Write(0x1E,0x74);
+
+  //Preamble length set to 8 symbols
+  //0x0008 + 4 = 12
+  RFM_Write(0x20,0x00);
+  RFM_Write(0x21,0x08);
+
+  //Set LoRa sync word
+  RFM_Write(0x39,0x34);
+
+  RFM_Write(0x33,0x27);
+  RFM_Write(0x3B,0x19);
+
+  //Set FIFO pointers
+  //TX base adress
+  RFM_Write(0x0E,0x80);
+  //Rx base adress
+  RFM_Write(0x0F,0x00);
+
+}
+
+/*
+*****************************************************************************************
+* Description : Function for sending a package with the RFM
+*
+* Arguments   : *RFM_Tx_Package Pointer to arry with data to be send
+*         Package_Length  Length of the package to send
+*****************************************************************************************
+*/
+
+void RFM_Send_Package(unsigned char *RFM_Tx_Package, unsigned char Package_Length)
+{
+  unsigned char i;
+  unsigned char RFM_Tx_Location = 0x00;
+
+  //Switch DIO0 to TxDone
+  RFM_Write(0x40,0x40);
+
+  //Set payload length to the right length
+  RFM_Write(0x22,Package_Length);
+
+  //Get location of Tx part of FiFo
+  RFM_Tx_Location = RFM_Read(0x0E);
+
+  //Set SPI pointer to start of Tx part in FiFo
+  RFM_Write(0x0D,RFM_Tx_Location);
+
+  //Write Payload to FiFo
+  for (i = 0;i < Package_Length; i++)
+  {
+    RFM_Write(0x00,*RFM_Tx_Package);
+    RFM_Tx_Package++;
+  }
+
+  //Switch RFM to Tx
+  RFM_Write(0x01,0x83);
+
+  //Wait for TxDone
+  while(digitalRead(DIO0) == LOW)
+  {
+  }
+
+  //Clear interrupt
+  RFM_Write(0x12,0x08);
+
+  //Switch RFM to Standby
+  RFM_Write(0x01,0x81);
+
+  //Wait for Mode Ready
+  while (digitalRead(DIO5) == LOW)
+  {
+  }
+
+}
+
+/*
+*****************************************************************************************
+* Description : Funtion that reads a register from the RFM and returns the value
+*
+* Arguments   : RFM_Address Address of register to be read
+*
+* Returns   : Value of the register
+*****************************************************************************************
+*/
+
+unsigned char RFM_Read(unsigned char RFM_Address)
+{
+  unsigned char RFM_Data;
+
+  //Set NSS pin low to start SPI communication
+  digitalWrite(10,LOW);
+
+  //Send Address
+  SPI.transfer(RFM_Address);
+  //Send 0x00 to be able to receive the answer from the RFM
+  RFM_Data = SPI.transfer(0x00);
+
+  //Set NSS high to end communication
+  digitalWrite(10,HIGH);
+
+  //Return received data
+  return RFM_Data;
+}
+
+/*
+*****************************************************************************************
+* Description : Funtion that writes a register from the RFM
+*
+* Arguments   : RFM_Address Address of register to be written
+*         RFM_Data    Data to be written
+*****************************************************************************************
+*/
+
+void RFM_Write(unsigned char RFM_Address, unsigned char RFM_Data)
+{
+  //Set NSS pin Low to start communication
+  digitalWrite(10,LOW);
+
+  //Send Addres with MSB 1 to make it a writ command
+  SPI.transfer(RFM_Address | 0x80);
+  //Send Data
+  SPI.transfer(RFM_Data);
+
+  //Set NSS pin High to end communication
+  digitalWrite(10,HIGH);
+}
