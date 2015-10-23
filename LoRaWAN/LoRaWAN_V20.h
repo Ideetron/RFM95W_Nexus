02ce60380c62db2acc9482ddd61beca509380b88
Gerben <gerben@ideetron.nl>
2015-10-23 15:23:03 +0200

    Create LoRaWAN_V20.h

---
+++ LoRaWAN/LoRaWAN_V20.h
@@ -0,0 +1,42 @@
+/******************************************************************************************
+*
+* File:        LoRaWAN_V20.h
+* Author:      Gerben den Hartog
+* Compagny:    Ideetron B.V.
+* Website:     http://www.ideetron.nl/LoRa
+* E-mail:      info@ideetron.nl
+******************************************************************************************/
+
+/****************************************************************************************
+*
+* Created on: 			  23-10-2015
+* Supported Hardware:	ID150119-02 Nexus board with RFM95
+*
+* History:
+*
+* Firmware version: 1.0
+* First version for LoRaWAN
+* 
+* Firmware version: 2.0
+* Ported to Arduino using own AES encryption
+****************************************************************************************/
+
+#ifndef LORAWAN_V20_H
+#define LORAWAN_V20_H
+
+/*
+*****************************************************************************************
+* DEFINES
+*****************************************************************************************
+*/
+
+#define DS2401  2
+#define MFP     3
+#define DIO0    4
+#define DIO1    5 
+#define DIO5    6
+#define DIO2    7
+#define CS      8
+#define LED     9
+
+#endif
